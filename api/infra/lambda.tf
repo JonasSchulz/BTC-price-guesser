9dcf47e828257ad53a9436a9b582aa2d251ed899
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-10-29 11:36:28 +0000

    Fix lambda source path

--- api/infra/lambda.tf
+++ api/infra/lambda.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda_zip" {
     type          = "zip"
-    source_file   = "${path.module}/../src/index.js"
-    output_path   = "lambda_function.zip"
+    source_file   = "${path.module}/src/index.js"
+    output_path   = "${path.module}/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
