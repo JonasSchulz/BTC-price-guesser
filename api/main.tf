45dfdef28ba0ca3bebe24a1a4fc6c7cebe25b30c
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-10-29 18:59:17 +0000

    Fix lambda node runtime

--- api/main.tf
+++ api/main.tf
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "test_lambda" {
   role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
   handler          = "index.handler"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
-  runtime          = "nodejs6.10"
+  runtime          = "nodejs20.x"
 }
 
 resource "aws_iam_role" "iam_for_lambda_tf" {
