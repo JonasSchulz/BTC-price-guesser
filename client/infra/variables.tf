e34e5743d6956375d21f0be1802fa4e6e22e6124
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-11-02 14:45:47 +0000

    Move react app to amplify (#3)
    * Try Amplify
    
    * Enable deployment on branch
    
    * Add amplify service role
    
    * Add amplify admin default permissions
    
    * Remove the service role
    
    * Small UI change to test deployment

--- client/infra/variables.tf
+++ client/infra/variables.tf
@@ -7,3 +7,32 @@ variable "aws_region" {
   type = string
   default = "eu-central-1"
 }
+
+variable "github_token" {
+  type        = string
+  description = "github token to connect github repo"
+}
+
+variable "repository" {
+  type        = string
+  description = "github repo url"
+  default     = "https://github.com/JonasSchulz/BTC-price-guesser"
+}
+
+variable "app_name" {
+  type        = string
+  description = "AWS Amplify App Name"
+  default     = "btc-price-guesser-client"
+}
+
+variable "branch_name" {
+  type        = string
+  description = "AWS Amplify App Repo Branch Name"
+  default     = "main"
+}
+
+variable "domain_name" {
+  type        = string
+  default     = "awsamplifyapp.com" #change this to your custom domain
+  description = "AWS Amplify Domain Name"
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello! Current BTC price is:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Wanna guess where it is going?
+        </p>
       </header>
     </div>
   );
