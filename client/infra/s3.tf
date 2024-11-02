8553ff50c4215520bbaef9a04883ad930985febf
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-11-02 11:18:02 +0000

    Add client deployment to s3 bucket

--- client/infra/s3.tf
+++ client/infra/s3.tf
@@ -1,10 +1,44 @@
-resource "aws_s3_bucket" "react-aws-terraform-github-actions-s3-bucket" {
+resource "aws_s3_bucket" "my-static-website-html" {
   bucket = var.bucket_name
-  acl = "public-read"
-  policy = data.aws_iam_policy_document.s3-website-policy.json
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_public_access_block" "static_site_bucket_public_access" {
+  bucket = aws_s3_bucket.my-static-website-html.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
+  bucket = var.bucket_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.static_site_bucket_public_access]
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_bucket_website_config" {
+  bucket = aws_s3_bucket.my-static-website-html.id
+
+  index_document {
+    suffix = "index.html"
   }
 }
