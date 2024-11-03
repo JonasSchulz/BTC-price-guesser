7048ffd6f404dbb820c132e3bfd1a56da4f95cc4
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-11-03 21:48:27 +0000

    Add simply dynamodb query to get guesses lambda

--- api/src/getGuesses.ts
+++ api/src/getGuesses.ts
@@ -1,13 +1,43 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb"
 import type { Context, APIGatewayProxyStructuredResultV2, APIGatewayProxyEventV2, Handler } from "aws-lambda"
+
+const client = new DynamoDBClient({})
+const docClient = DynamoDBDocumentClient.from(client)
+
+type GetGuessesParams = {
+  user_name: string
+}
 
 export const handler: Handler = async (
   event: APIGatewayProxyEventV2,
   _context: Context,
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-  console.log(event.queryStringParameters)
+  const username = (event.queryStringParameters as GetGuessesParams).user_name
 
+  // if (!username)
+  //   return {
+  //     statusCode: 400,
+  //     body: "No username found",
+  //   }
+
+  // return {
+  //   statusCode: 200,
+  //   body: "No username found",
+  // }
+
+  const command = new QueryCommand({
+    TableName: "GuessesTable",
+    KeyConditionExpression: "user_name = :user_name",
+    ExpressionAttributeValues: {
+      ":user_name": username,
+    },
+  })
+
+  const response = await docClient.send(command)
+  console.log(response)
   return {
-    statusCode: 400,
-    body: "No username found",
+    statusCode: 200,
+    body: "response",
   }
 }
