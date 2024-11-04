e44e5efaebdee0089ed968be3de191f082aebe87
Jonas Schulz <jonas.d.schulz@gmail.com>
2024-11-04 13:19:38 +0000

    Create resolve guesses function

---
+++ api/src/resolveGueses.ts
@@ -0,0 +1,34 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient, ScanCommand, ScanCommandInput } from "@aws-sdk/lib-dynamodb"
+import { marshall } from "@aws-sdk/util-dynamodb"
+import type { Context, APIGatewayProxyStructuredResultV2, APIGatewayProxyEventV2, Handler } from "aws-lambda"
+
+const client = new DynamoDBClient({})
+const dbClient = DynamoDBDocumentClient.from(client)
+
+export const handler: Handler = async (
+  _event: APIGatewayProxyEventV2,
+  _context: Context,
+): Promise<APIGatewayProxyStructuredResultV2> => {
+  // scan DB for any pending guesses older than 1 minute
+  const input: ScanCommandInput = {
+    TableName: "GuessesTable",
+    FilterExpression: "attribute_not_exists(result) or resul = :null",
+    ExpressionAttributeValues: marshall({
+      ":null": null,
+    }),
+  }
+
+  const command = new ScanCommand(input)
+  const response = await dbClient.send(command)
+
+  console.log(response)
+  // if there are pending guesses, get the current BTC price
+
+  // score any pending guesses
+
+  return {
+    statusCode: 200,
+    body: "",
+  }
+}
---
+++ api/src/resolveGuesses.test.ts
@@ -0,0 +1,60 @@
+// ./src/lambda1.test.ts
+import assert from "node:assert"
+import { beforeEach, describe, it } from "node:test"
+import { APIGatewayProxyEventV2, Callback, Context } from "aws-lambda"
+import { mockClient } from "aws-sdk-client-mock"
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb"
+import { handler } from "./resolveGueses"
+
+const dynamoMock = mockClient(DynamoDBDocumentClient)
+
+beforeEach(() => {
+  dynamoMock.reset()
+})
+
+describe("resolveGuesses handler", () => {
+  it("resolves any pending guesses when invoked", async (_t) => {
+    dynamoMock.on(ScanCommand).resolves({
+      Items: [
+        {
+          guess: "decrease",
+          inserted_at: "1730662299137",
+          result: null,
+          user_name: "Jonas",
+        },
+        {
+          guess: "increase",
+          inserted_at: "1730662301474",
+          result: null,
+          user_name: "Jonas",
+        },
+      ],
+    })
+
+    const testEvent = {} as APIGatewayProxyEventV2
+    const testContext = {} as Context
+    const testCallback = {} as Callback
+
+    const response = await handler(testEvent, testContext, testCallback)
+
+    assert.strictEqual(dynamoMock.commandCalls(ScanCommand).length, 1)
+    assert.strictEqual(response.statusCode, 200)
+    assert.strictEqual(
+      response.body,
+      JSON.stringify([
+        {
+          guess: "decrease",
+          inserted_at: "1730662299137",
+          result: -1,
+          user_name: "Jonas",
+        },
+        {
+          guess: "increase",
+          inserted_at: "1730662301474",
+          result: 1,
+          user_name: "Jonas",
+        },
+      ]),
+    )
+  })
+})
